016397dafdfc7caa45f29a008978ef6101ea9a8b
unknown <arpitpaliwal14@gmail.com>
2021-03-21 20:54:25 +0530

    initial commit

---
+++ APITest/APIRegression.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecGlowGitTest;
+namespace APITest
+{
+    [TestClass]
+    public class APIRegression
+    {
+        [TestMethod]
+        public void VerifyUserList()
+        {
+            var demo = new Demo();
+
+        }
+    }
+}
---
+++ SpecGlowGitTest/Demo.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+
+namespace SpecGlowGitTest
+{
+    public class Demo
+    {
+        public UserListDTO GetUsers()
+        {
+            var restclient = new RestClient("https://reqres.in/");
+            var restRequest = new RestRequest("api/users?page=2", Method.GET);
+            restRequest.AddHeader("Accept", "application/json");
+            restRequest.RequestFormat = DataFormat.Json;
+
+            IRestResponse response = restclient.Execute(restRequest);
+            var content = response.Content;
+
+            var users = JsonConvert.DeserializeObject<UserListDTO>(content);
+            return users;
+        }
+    }
+}
